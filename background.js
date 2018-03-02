2cb56e5be35c78daa4594d3b9ba9b334bd6a9307
Jorand <jorand.lepape@gmail.com>
2018-03-02 17:23:02 +0100

    Retry when suspend tab is activated, improve comments and logs

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-    chrome.tabs.sendMessage(tabId, {type: 'changePage', data: tab}, function (doc) {
+// Fired when a tab is updated
+chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+  if (info.status === 'complete' && tab.active === true) {
+    chrome.tabs.sendMessage(tabId, {type: info.status, data: tab}, function (doc) {
       //console.log(doc);
     });
   }
 });
+
+// Fires when the active tab in a window changes
+// - For suspend tab or new tab not activated
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.sendMessage(activeInfo.tabId, {type: "active", data: activeInfo}, function (doc) {
+    //console.log(doc);
+  });
+}); 
--- content.js
+++ content.js
@@ -1,14 +1,14 @@
 (function () {
 
-$(document).ready(init);
+$(document).ready(init); // Content Ready
 
-var RETRY_DELAY_MS = 800; // time between retry
-var MAX_RETRY = 3;
-var SUCESS_MIN = 2;
+var RETRY_DELAY_MS = 800;	// Time between retry
+var MAX_RETRY = 3;				// Number of retry
+var SUCESS_MIN = 2;				// Minimum match to validate sucess
 
 var timer,
 		retryCount = 0,
-		sucessCount = 0, // Count of link found in one time
+		sucessCount = 0, // Count of link found in one retry loop
 		options = {},
 		link = '',
 		layout = '2015',
@@ -19,14 +19,11 @@ function init() {
 
 	clearTimeout(timer);
 	timer = setTimeout(function() {
-
-		console.log("[INFO] utip4yt: Testing…");
-
+		//console.log("[INFO] utip4yt: Testing…");
 		link = '';
-
-		$("paper-button#more").trigger("click"); //.yt-uix-expander-body
-
-		link = $('a[href*="utip.io"]').first().attr("href");
+		
+		$("#more").trigger("click"); // Open description
+		
 		// Look for a utip link
 		// - #watch-description-text (layout version 2015) 
 		// - #content, #description (new youtube version)
@@ -34,19 +31,20 @@ function init() {
 		link = $description.find('a[href*="utip.io"]').first().attr("href");
 
 		if (link) {
-			console.log("[INFO] utip4yt: utip link found");
+			//console.log("[INFO] utip4yt: utip link found");
 			sucessCount++;
 		}
 		else {
 			sucessCount = 0;
-			console.log("[INFO] utip4yt: no utip link found");
+			//console.log("[INFO] utip4yt: no utip link found");
 			reset();
 		}
 
 		if (sucessCount >= SUCESS_MIN) {
 			setLayout();
-			console.log("[INFO] utip4yt: layout version "+layout);
-
+			console.log("[INFO] utip4yt: utip link found");
+			console.log("[INFO] utip4yt: layout version " + layout);
+			
 			var button = '<div class="utip4yt-container"><a class="utip4yt-button"><svg role="img" aria-label="Utip for youtube button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" enable-background="new 0 0 128 128"><path stroke="#FFA644" stroke-width="30" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M93 27v47.1c0 4.1-.8 7.9-2.2 11.5-4.3 10.6-14.6 18-26.8 18-16.2 0-29-13.2-29-29.4v-47.2" fill="none"/><path stroke="#26FF82" stroke-width="30" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M90.8 85.5c-4.3 10.6-14.6 18-26.8 18-16.2 0-29-13.2-29-29.4v-47.1" fill="none"/><path stroke="#BF3BFF" stroke-width="30" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" fill="none" d="M35 74v-47"/><circle fill="#AE29FC" cx="35" cy="74.5" r="15"/><circle fill="#F99430" cx="93" cy="27" r="15"/><circle fill="#24EA74" cx="91" cy="85" r="15"/><title>Utip for youtube</title><desc>Automatically open a popup when you like a youtube video with a utip link in description.</desc></svg></a></div>';
 
 			if (layout == '2015') {
@@ -60,12 +58,16 @@ function init() {
 		else {
 			if (retryCount < MAX_RETRY) {
 				retryCount++;
-				console.log("[INFO] utip4yt: retry ("+retryCount+")");
+				//console.log("[INFO] utip4yt: retry ("+retryCount+")");
 				init();
 			}
 			else {
 				if (sucessCount != 0) {
+					// try again if at least one link has been found once
 					refresh();
+				}
+				else {
+					console.log("[INFO] utip4yt: no utip link found");
 				}
 			}
 		}
@@ -112,7 +114,7 @@ function reset() {
 };
 
 function refresh() {
-	console.log("refresh");
+	console.log("[INFO] utip4yt: looking for a utip link…");
 	reset();
 	retryCount = 0;
 	sucessCount = 0;
@@ -120,6 +122,7 @@ function refresh() {
 };
 
 function getOptions() {
+	// Get extension options
 	chrome.storage.sync.get({
 		thumbUpOption: true
 	}, function(items) {
@@ -128,7 +131,9 @@ function getOptions() {
 }
 
 chrome.extension.onMessage.addListener(function(request, sender, response) {
-	if (request.type === 'changePage') {
+	// listen to messages sent by the background script
+	console.log("[MSG] utip4yt: ", request.type);
+	if (request.type === 'complete' || request.type === 'active') {
 		refresh();
 	}
 	return true;
